5d9cce6d69460149736c7cc6a7b58288814cd711
DecrepitSloth <Knowlesaa77@gmail.com>
2023-04-30 12:36:02 -0500

    add variables to lines 21, 22, 24, and 68. add concat to numbers(). add if else statement to operators(). update clearDisplay. add comments to script.js

--- script.js
+++ script.js
@@ -18,26 +18,54 @@ const ops = {
     divide: "/"
 };
 
+let equationArray = [];
+let equationString = "";
 let display = "";
+let operator = "";
 
 function numbers(inputNum) {
+    // send input to display
     display += inputNum;
     document.getElementById("output").innerHTML = display;
+    // send input to equationString
+    equationString += inputNum;
 }
 
 function operators(inputOp) {
+    // send input to display
     display += inputOp;
     document.getElementById("output").innerHTML = display;
+    // send input to operator
+    operator = inputOp;
+    console.log(operator)
+
+    if (firstNum == 0) {
+        // take all numbers input and make them firstNum
+        firstNum = parseInt(equationString);
+        equationString = "";
+        console.log(firstNum);
+    } else {
+        // take all number inputs after using an operator and put them in secondNum
+        secondNum = parseInt(equationString);
+        equationString = "";
+        console.log(secondNum);
+    }
 }
 
 function clearDisplay() {
     display = "";
     document.getElementById("output").innerHTML = "";
+    equationArray = [];
+    equationString = "";
+    firstNum = 0;
+    secondNum = 0;
+    operator = "";
 }
 
 // Currently not set up to update from nums
 let firstNum = 0;
 let secondNum = 0;
+let total = 0;
 
 // remember to add return or equivalent for each function
 let add = (firstNum, secondNum) => firstNum + secondNum;
